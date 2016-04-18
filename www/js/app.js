87d7cf810fc44bebbc33e32db8c75f7521782c5b
huyhung1905 <huyhung1905@gmail.com>
2016-04-18 08:57:44 +0700

    test thử

--- www/js/app.js
+++ www/js/app.js
@@ -81,5 +81,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
-
+var i=0;
+i++;
+console.log(i);
 });
